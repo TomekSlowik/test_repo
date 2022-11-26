b23d9adf484e071903eebec9f7845b84c6d6ba6b
Tomula <tomek.slowik75@gmail.com>
2022-11-26 11:53:09 +0100

    Zachęta podania liczby

--- app/src/Main.java
+++ app/src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("Hello Tomasz!");
+        while (true){
+            System.out.println("Podaj liczbę do policzenia pierwiastka");
+        }
+
     }
 }
