b1dcd5d69f2227709d6d9bdf711f809500c10a88
Tomula <tomek.slowik75@gmail.com>
2022-11-26 12:02:35 +0100

    Liczenie pierwiastka

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("Hello Tomasz!");
         while (true){
             System.out.println("Podaj liczbę do policzenia pierwiastka");
+            Scanner scanner = new Scanner(System.in);
+            int liczba = scanner.nextInt();
+            System.out.println(Math.sqrt((double) liczba));
         }
 
     }
